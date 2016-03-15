3a58c24977d097603741390b23665fd1516a8e29
LeonardoPetrucci <leonardo.petrucci94@gmail.com>
2016-03-15 16:24:20 +0100

    Added files via upload

---
+++ messages.h
@@ -0,0 +1,15 @@
+//
+// Created by Leonardo on 15/03/2016.
+//
+
+#ifndef TALK_MESSAGES_H
+#define TALK_MESSAGES_H
+
+#define HELPMESSAGE "Puoi mandare questi comandi:\n$list - invia la lista degli utenti disponibili per una conversazione\n$quit - esce dalla chat\n$connection USERNAME - inizia una chat session con l'utente USERNAME se possibile\n$quit_connection - permette di uscire da una chat session\n"
+#define WELCOME     "Benvenuto! Per iniziare a utilizzare il servizio devi creare un nome utente (max 20 caratteri): "
+#define LISTMESS    "Seleziona il parner con cui avviare una chat."
+#define UNREACHABLE "Il partner selezionato non è disponibile."
+#define BYE         "Stai uscendo dal server... Arrivederci!"
+#define CLOSECHAT   "Fine della conversazione"
+
+#endif //TALK_MESSAGES_H
