d0db90636fc6e8a7f2d7651dc3d3c4d0aa95dcdb
marcozecchini <zecchini.1596071@studenti.uniroma1.it>
2016-03-20 11:56:35 +0100

    Update chat.h

--- server/chat.h
+++ server/chat.h
@@ -5,7 +5,7 @@
 #ifndef TALK_CHAT_H
 #define TALK_CHAT_H
 
-
+#include "structs.h"
 /*
 	Read and return the number of bytes read from the socket
 */
@@ -14,6 +14,15 @@ size_t  ReadSocket(int ds, char buf[], int n);
 	Write and return the number of bytes written from the socket
 */
 size_t  WriteSocket(int ds, char buf[], int n);
+
+// Funzione ausiliaria che trova dato lo username il partner per una chat_session
+
+int trovaPartner(char username[], client_info* list);
+
+// Esegue tutte le operazioni preliminari per l'inizio di una chat session
+
+void startChatSession(int sock,client_info* list, int pos, char username[]);
+
 /*
  * Gestisce la chat_session tra due utenti. Avrà minimo due parametri in ingresso che sono le struct con le informazioni sugli user.
  *//*
