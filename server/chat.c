3c75f6754e1e49c7ca2aa13fdf70e44a46d5db6f
marcozecchini <zecchini.1596071@studenti.uniroma1.it>
2016-03-20 11:55:46 +0100

    Update chat.c

--- server/chat.c
+++ server/chat.c
@@ -9,6 +9,10 @@
 #include <string.h>
 
 #include "chat.h"
+#include "structs.h"
+#include "macros.h"
+#include "messages.h"
+#include "commands.h"
 /*guardando il codice https://gist.github.com/silv3rm00n/5821760, la write ha una gestione più "elementare" della
  * read, che invece necessita dopo l'operazione di aggiungere il terminatore di stringa '\0' alla posizione
  * buffer[byte_letti]. Quando dobbiamo inviare le stringhe definite in messages.h facciamo la semplice write,
@@ -45,4 +49,39 @@ size_t WriteSocket(int ds, char buf[], int n){
         n -= ret;
     }
     return written_bytes;
+}
+
+int trovaPartner(char username[], client_info* list){
+    int i;
+    for (i = 0; i < MAX_USERS; i++){
+        if (strcmp(list[i].name, username)== 0){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void startChatSession(int socket,client_info* list, int pos, char username[]) {
+
+    int ret;
+
+    //devo trovare la struct info con quello username
+    int  altro_pos = trovaPartner(username,list);
+
+    if (altro_pos == -1){
+        char* not_found = "Utente non trovato\n";
+        ret = WriteSocket(socket,not_found,strlen(not_found));
+        ERROR_HELPER(ret, "Errore in 'Utente non Trovato'");
+        return;
+    }
+
+    char* found="";
+    sprintf(found,"Utente %s trovato! Ti mando in chat_session!\n",list[pos].name);
+    ret = WriteSocket(socket, found,strlen(found));
+
+    //lancio funzione che gestisce la chat session..
+
+    //devo inviare segnale a thread partner
+    //PROSEGUI DA QUI
+
 }
